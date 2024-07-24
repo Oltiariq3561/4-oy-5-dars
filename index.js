194dc35560be918fb690c16006a96b625da09d02
Oltiariq3561 <oltiariq356@gmail.com>
2024-07-24 14:45:15 -0700

    16ta tugadi

---
+++ index.js
@@ -0,0 +1,160 @@
+// let a=+prompt('son');
+// let max = a;
+// let min = a
+// for(let i = 1; i<=4; i++){
+//     a=+prompt('son');
+//     if (a>max) {
+//         max = a
+//     }
+//     if(a<min){
+//     min = a
+//     }
+
+// }
+// console.log(max,'kotta');
+// console.log(min,'kichik');
+
+// let n = 1234;
+// reversed = 0;
+// while (n > 0) {
+//     reversed = reversed * 10 + n % 10;
+//     n = Math.floor(n / 10);
+// }
+// console.log(reversed);
+
+// let fruits = ['olma' , 'kivi' , 15 , 'ananas'];
+// fruits[2]='banan'
+// fruits.push(1234);
+// fruits.pop();
+// fruits.unshift('deidei');
+// fruits.shift();
+// console.log(fruits);
+
+//  let a = [3,7,4,9,3,5,7];
+//  let sum = 0;
+//  for(let i = 0; i < a.length; i++){
+// sum+=a[i]
+// }
+// console.log(sum);
+
+// UYGA VAZIFA
+
+// 1-masala
+// let arg = [1,2,3,4];
+// let res = [];
+// for(let i = 0; i<arg.length; i++){
+// res.push(arg[i] * 2);
+// }
+// console.log(res);
+
+// 2-masala
+// let arg = [1,2,3,4];
+// let reversed = arg.reverse();
+// console.log(reversed);
+
+// 3-masala
+// let arr = [1,2,3,4,5,-6,7,-8];
+// let res = [];
+// for(let i = 0; i < arr.length; i++){
+// if(arr[i]>0)
+// res.push(arr[i])
+// }
+// console.log(res);
+
+// 4-masala
+// let arr = [1,2,3,4,5,7];
+// let res = 0
+// for(let i = 0; i < arr.length; i++){
+// res = res + arr[i]
+// }
+// console.log(res);
+
+// 5-masala
+// let arr = [1,2,3,4,5,7];
+// arr.push('salom');
+// console.log(arr);
+
+// 6-masala
+// let arr = [1,2,3,4,5,7];
+// arr.pop()
+// console.log(arr);
+
+// 7-masala va 8-masala
+// let arr = [1,2,3,4,5,7];
+// arr.unshift('salom')
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+
+// 9-masala
+// let arr = [1,2,3,4,5,7];
+// let index = arr.indexOf(3);
+// console.log(index);
+
+// 10-masala
+// let arr = [1,2,3,4,5,7];
+// let kochirildi = []; 
+// for(let i = 0 ; i<arr.length; i++){
+//     kochirildi.push(arr[i]);
+// }
+// console.log(kochirildi);
+
+// 11-masala
+// let arr = [1,2,3,4,5,7];
+// let max = 0; 
+// for(let i = 0 ; i<arr.length; i++){
+//     if(arr[i] > max){
+//     max = arr[i]
+//     }
+// }
+// console.log(max);
+
+// 12-masala
+// let arr = [1,2,3,4,5,7];
+// let min = 1; 
+// for(let i = 0 ; i<arr.length; i++){
+//     if(arr[i] < min){
+//     min = arr[i]
+//     }
+// }
+// console.log(min);
+
+// 13-masala
+// let arr = [1,2,3,4,5,7];
+// let generator = 0; 
+// for(let i = 0 ; i<arr.length; i++){
+//     if(arr[i] % 2 == 0){
+//     generator++
+//     }
+// }
+// console.log(generator);
+
+// 14-masala
+// let arr = [1,2,3,4,5,7];
+// let generator = 0; 
+// for(let i = 0 ; i<arr.length; i++){
+//     if(arr[i] % 2 == 1){
+//     generator++
+//     }
+// }
+// console.log(generator);
+
+// 15-masala
+// let arr = [1,2,0,3,4,0,5,7];
+// let kottalar = []; 
+// for(let i = 0 ; i<arr.length; i++){
+//     if(arr[i] > 0){
+//     kottalar.push(arr[i])
+//     }
+// }
+// console.log(kottalar);
+
+// 16-masala
+// let arr = [1, 2, 0, 3, 4, 0, 5, 7];
+// let res = [];
+// for (let i = 0; i < arr.length; i++) {
+//     res.push(arr[i]);
+//     res.push(arr[i]);
+// }
+// console.log(res);
+
